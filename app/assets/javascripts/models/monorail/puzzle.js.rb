4bf8785d5d91458b6fc04d0624147be33dc3ee26
Doug Orleans <dougorleans@gmail.com>
2015-01-07 01:45:20 -0500

    Generalize to square puzzles of all sizes.

--- app/assets/javascripts/models/monorail/puzzle.js.rb
+++ app/assets/javascripts/models/monorail/puzzle.js.rb
@@ -4,17 +4,27 @@ module Monorail
   class Puzzle < Vienna::Model
     attributes :dots, :lines
 
-    def initialize
-      self.dots = (0..1).map do |row|
-        (0..1).map do |col|
+    def initialize(size = 2)
+      maxrow = size - 1
+      self.dots = (0..maxrow).map do |row|
+        maxcol = size - 1
+        if size.odd?
+          # To make an even number of dots total, omit the last dot of the last row.
+          maxcol -= 1 if row == maxrow
+        end
+        (0..maxcol).map do |col|
           Dot.new(row: row, col: col)
         end
       end
 
-      self.lines = [connect(dots[0][0], dots[0][1]),
-                    connect(dots[0][0], dots[1][0]),
-                    connect(dots[0][1], dots[1][1]),
-                    connect(dots[1][0], dots[1][1])]
+      self.lines = []
+      (0...dots.length).each do |r|
+        row = dots[r]
+        (0...row.length).each do |c|
+          lines << connect(dots[r][c], dots[r][c+1]) unless c+1 == row.length
+          lines << connect(dots[r][c], dots[r+1][c]) unless r+1 == dots.length || c == dots[r+1].length
+        end
+      end
     end
 
     private
--- test/javascripts/models/monorail/puzzle_test.js.rb
+++ test/javascripts/models/monorail/puzzle_test.js.rb
@@ -6,25 +6,67 @@ module Monorail
       assert_equal %i(dots lines), Puzzle.columns
     end
 
-    test 'dots form a grid' do
-      dots = Puzzle.new.dots
-      assert_equal 2, dots.length
-      dots.each_with_index do |row, r|
-        assert_equal 2, row.length
+    test 'size 2' do
+      subject = Puzzle.new
+      assert_has_grid_of_dots subject, 2
+      assert_equal 4, subject.lines.length
+      assert_adjacent_dots_are_connected subject
+    end
+
+    test 'size 3' do
+      subject = Puzzle.new(3)
+      assert_bottom_right_dot_is_omitted subject, 3
+      assert_equal 10, subject.lines.length
+      assert_adjacent_dots_are_connected subject
+    end
+
+    test 'size 4' do
+      subject = Puzzle.new(4)
+      assert_has_grid_of_dots subject, 4
+      assert_equal 24, subject.lines.length
+      assert_adjacent_dots_are_connected subject
+    end
+
+    test 'size 5' do
+      subject = Puzzle.new(5)
+      assert_bottom_right_dot_is_omitted subject, 5
+      assert_equal 38, subject.lines.length
+      assert_adjacent_dots_are_connected subject
+    end
+
+    private
+
+    def assert_has_grid_of_dots(puzzle, size)
+      assert_equal size, puzzle.dots.length
+      puzzle.dots.each_with_index do |row, r|
+        assert_equal size, row.length
         row.each_with_index do |dot, c|
-          assert_equal r, dot[:row]
-          assert_equal c, dot[:col]
+          assert_equal r, dot.row
+          assert_equal c, dot.col
         end
       end
     end
 
-    test 'lines form a lattice' do
-      lines = Puzzle.new.lines
-      assert_equal 4, lines.length
-      assert_equal 2, lines.select { |line| line.dot1[:row] == line.dot2[:row] }.length
-      assert_equal 2, lines.select { |line| line.dot1[:col] == line.dot2[:col] }.length
-      assert_equal 2, lines.select { |line| line.dot1[:row] == 0 && line.dot1[:col] == 0 }.length
-      assert_equal 2, lines.select { |line| line.dot2[:row] == 1 && line.dot2[:col] == 1 }.length
+    def assert_bottom_right_dot_is_omitted(puzzle, size)
+      assert_equal size, puzzle.dots.length
+      assert_equal size, puzzle.dots.first.length
+      assert_equal size-1, puzzle.dots.last.length
+    end
+
+    def assert_adjacent_dots_are_connected(puzzle)
+      dots = puzzle.dots
+      lines = puzzle.lines
+      (0...dots.length).each do |r|
+        row = dots[r]
+        (0...row.length).each do |c|
+          unless c+1 == row.length
+            assert_equal 1, lines.select { |line| line.dot1 == dots[r][c] && line.dot2 == dots[r][c+1] }.length
+          end
+          unless r+1 == dots.length || c == dots[r+1].length
+            assert_equal 1, lines.select { |line| line.dot1 == dots[r][c] && line.dot2 == dots[r+1][c] }.length
+          end
+        end
+      end
     end
   end
 end
