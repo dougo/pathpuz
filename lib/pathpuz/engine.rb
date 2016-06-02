054c1c9a0b61d428704876cb5ad6e1ebcd667774
Doug Orleans <dougorleans@gmail.com>
2016-06-02 13:53:22 -0400

    Omit app/assets from eager_load_paths, so it doesn't try to load Opal files in Ruby!

--- lib/pathpuz/engine.rb
+++ lib/pathpuz/engine.rb
@@ -7,5 +7,8 @@ require 'opal-vienna'
 module Pathpuz
   class Engine < Rails::Engine
     isolate_namespace Pathpuz
+
+    # Don't eager-load the .js.rb files in app/assets!
+    config.eager_load_paths -= ["#{root}/app/assets"]
   end
 end
