3495e8028d879c4e0539c6b55e10084e7e3b2f84
Doug Orleans <dougorleans@gmail.com>
2016-06-26 12:17:07 -0400

    Don't rely on order of dot elements.

--- test/features/monorail_test.rb
+++ test/features/monorail_test.rb
@@ -18,14 +18,14 @@ class MonorailTest < Capybara::Rails::TestCase
   end
 
   test 'solve the trivial puzzle by clicking on dots' do
-    dots.first.click
-    dots.last.click
+    click_dot(0, 0)
+    click_dot(1, 1)
     assert_equal 4, black_lines.length
     assert_text page, 'SOLVED'
   end
 
   test 'skip to the next puzzle' do
-    click_line(0, 1)
+    click_line([0,0], [0,1])
     assert_equal 1, black_lines.length
     next_puzzle!
     assert_equal 8, dots.length
@@ -35,30 +35,27 @@ class MonorailTest < Capybara::Rails::TestCase
 
   test 'solve the 3x3 puzzle' do
     next_puzzle!
-    # Dots are numbered like so:
-    # 0---1---2
-    # |   |   |
-    # 3---4---5
-    # |   |
-    # 6---7
-    click_line(0, 1)
-    click_line(1, 2)
-    click_line(2, 5)
-    click_line(4, 5)
-    click_line(4, 7)
-    click_line(6, 7)
-    click_line(3, 6)
+    click_line([0,0], [0,1])
+    click_line([0,1], [0,2])
+    click_line([0,2], [1,2])
+    click_line([1,1], [1,2])
+    click_line([1,1], [2,1])
+    click_line([2,0], [2,1])
+    click_line([1,0], [2,0])
     refute_text page, 'SOLVED'
-    click_line(0, 3)
+    click_line([0,0], [1,0])
     assert_equal 8, black_lines.length
     assert_text page, 'SOLVED'
   end
 
   test 'solve the 3x3 puzzle by clicking on dots' do
     next_puzzle!
-    click_dots(0, 2, 5, 6)
+    click_dot(0, 0)
+    click_dot(0, 2)
+    click_dot(1, 2)
+    click_dot(2, 0)
     refute_text page, 'SOLVED'
-    click_dots(7)
+    click_dot(2, 1)
     assert_equal 8, black_lines.length
     assert_text page, 'SOLVED'
   end
@@ -70,32 +67,40 @@ class MonorailTest < Capybara::Rails::TestCase
     assert_equal 21, lines_to_click.length
     assert_equal 3, gray_lines.length
     # Corners:
-    click_dots(0, 3, 12, 15)
+    click_dot(0, 0)
+    click_dot(0, 3)
+    click_dot(3, 0)
+    click_dot(3, 3)
     # Completed dots:
-    click_dots(1, 2, 10)
+    click_dot(0, 1)
+    click_dot(0, 2)
+    click_dot(2, 2)
     # Dots with only one unknown:
-    click_dots(11, 14)
+    click_dot(2, 3)
+    click_dot(3, 2)
     # More completed dots:
-    click_dots(7, 13)
+    click_dot(1, 3)
+    click_dot(3, 1)
     # Another dot with only one unknown:
-    click_dots(6)
+    click_dot(1, 2)
 
     # The grid now looks like this:
-    #  0---1---2---3
-    #  |   x   x   |
-    #  4   5---6 x 7
-    #          |   |
-    #  8   9--10 x 11
-    #  |   x   x   |
-    # 12--13--14---15 
+    # o---o---o---o
+    # |   x   x   |
+    # o   o---o x o
+    #         |   |
+    # o   o---o x o
+    # |   x   x   |
+    # o---o---o---o 
 
     assert_equal 6, red_exes.length
     assert_equal 11, black_lines.length
     refute_text page, 'SOLVED'
 
-    # If lines 4,8 and 5,9 were present, there would be two separate loops, so 4,5 and 8,9 must be present.
-    click_line(4, 5)
-    click_line(8, 9)
+    # If lines [1,0],[2,0] and [1,1],[2,1] were present, there would be two separate loops,
+    # so [1,0],[1,1] and [2,0],[2,1] must be present.
+    click_line([1,0], [1,1])
+    click_line([2,0], [2,1])
     assert_text page, 'SOLVED'
   end
 
@@ -105,31 +110,29 @@ class MonorailTest < Capybara::Rails::TestCase
     @dots ||= all('circle')
   end
 
-  def click_dots(*indices)
-    indices.each { |i| dots[i].click }
+  def dot(row, col)
+    find("circle[cx='#{col}'][cy='#{row}']")
+  end
+
+  def click_dot(row, col)
+    dot(row, col).click
   end
 
   def lines_to_click
     @lines_to_click ||= all('line[cursor="pointer"]')
   end
 
-  def dot_center(dot)
-    circle = dots[dot]
-    [circle['cx'], circle['cy']]
+  def dot_rect(row, col)
+    page.evaluate_script("$('circle[cx=\"#{col}\"][cy=\"#{row}\"]')[0].getBoundingClientRect()")
   end
 
-  def dot_rect(x, y)
-    page.evaluate_script("$('circle[cx=\"#{x}\"][cy=\"#{y}\"]')[0].getBoundingClientRect()")
-  end
-
-  def dot_midpoint(dot)
-    x, y = dot_center(dot)
-    rect = dot_rect(x, y)
+  def dot_midpoint(row, col)
+    rect = dot_rect(row, col)
     [rect['left'] + rect['width']/2, rect['top'] + rect['height']/2]
   end
 
   def line_midpoint(dot1, dot2)
-    m1, m2 = dot_midpoint(dot1), dot_midpoint(dot2)
+    m1, m2 = dot_midpoint(*dot1), dot_midpoint(*dot2)
     [(m1[0]+m2[0])/2, (m1[1]+m2[1])/2]
   end
 
