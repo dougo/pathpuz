8bd60e19e9a413fd1b9693acf50bb6e6f817fcf7
Doug Orleans <dougorleans@gmail.com>
2016-07-04 00:42:36 -0400

    Undo previous commit... fixed it in opal-minitest instead.

--- test/javascripts/test_helper.js.rb
+++ test/javascripts/test_helper.js.rb
@@ -8,6 +8,3 @@ class Minitest::Test
   # Add the 'test' DSL.
   extend ActiveSupport::Testing::Declarative
 end
-
-# TODO: get this from the environment? I don't know why Opal doesn't set this...
-Encoding.default_external = 'UTF-8'
